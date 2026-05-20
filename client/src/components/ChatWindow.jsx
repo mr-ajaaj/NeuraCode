d2fe106fa91bce6fa69bdac823e792e54566d578
mr-ajaaj <mohamedridaajaaj25@gmail.com>
2026-05-21 00:00:17 +0100

    feat: create chat window component

--- client/src/components/ChatWindow.jsx
+++ client/src/components/ChatWindow.jsx
@@ -1,3 +1,17 @@
+import InputBox from "./InputBox";
+import Message from "./Message";
+
 export default function ChatWindow() {
-    
+  return (
+    <div className="flex flex-col flex-1">
+      
+      {/* Messages */}
+      <div className="flex-1 p-4 overflow-y-auto space-y-4">
+        <Message text="Hello 👋 I'm NeuraCode" isUser={false} />
+      </div>
+
+      {/* Input */}
+      <InputBox />
+    </div>
+  );
 }
