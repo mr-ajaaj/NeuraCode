096c623585d41a37990571ed77affac2c985aa1f
mr-ajaaj <mohamedridaajaaj25@gmail.com>
2026-05-20 20:00:09 +0100

    feat: create main application layout

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,14 @@
+import ChatWindow from "./components/ChatWindow";
+import Sidebar from "./components/Sidebar";
 
 function App() {
- 
-
+  
   return (
-    <>
-      
-    </>
-  )
+    <div className="flex h-screen bg-gray-900 text-white">
+      <Sidebar />
+      <ChatWindow />
+    </div>
+  );
 }
 
-export default App
+export default App;
