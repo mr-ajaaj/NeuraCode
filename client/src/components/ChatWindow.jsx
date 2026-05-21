23c742a265d60b58562e21ca5e1ba7c5d57619c2
mr-ajaaj <mohamedridaajaaj25@gmail.com>
2026-05-21 14:49:57 +0100

    feat: connect frontend to backend API

--- client/src/components/ChatWindow.jsx
+++ client/src/components/ChatWindow.jsx
@@ -7,18 +7,35 @@ export default function ChatWindow() {
     { text: "Hello 👋 I'm NeuraCode", isUser: false },
   ]);
 
-  const handleSendMessage = (text) => {
-    const newMessage = {
-      text: text,
-      isUser: true,
-    };
+  const handleSendMessage = async (text) => {
+    const userMessage = { text, isUser: true };
+    setMessages((prev) => [...prev, userMessage]);
 
-    setMessages([...messages, newMessage]);
+    try {
+      const res = await fetch("http://localhost:5000/chat", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ message: text }),
+      });
+
+      const data = await res.json();
+
+      const aiMessage = {
+        text: data.reply,
+        isUser: false,
+      };
+
+      setMessages((prev) => [...prev, aiMessage]);
+    } catch (err) {
+      console.error(err);
+    }
   };
 
   return (
     <div className="flex flex-col flex-1">
-
+      
       {/* Messages */}
       <div className="flex-1 p-4 overflow-y-auto space-y-4">
         {messages.map((msg, index) => (
